99e296bd62e1cbfe6c995d87643c6bbd5b97c401
Denyz Pylypenko <denyz.pylypenko@gmail.com>
2022-04-16 23:51:47 +0300

    map setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import { ComposableMap, Geographies, Geography } from "react-simple-maps";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+class Map extends Component {
+  handleGeographyClick = (geography) => {
+    console.log(geography);
+  };
+
+  render() {
+    return (
+      <div>
+        <ComposableMap
+          projectionConfig={{ scale: 2700, center: [31.4, 48.4] }}
+          height={400}
+          style={{ width: "100%", height: "auto" }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <Geographies geography={require("./gadm40_UKR_1.json")}>
+            {({ geographies }) =>
+              geographies.map((geo) => (
+                <Geography
+                  key={geo.rsmKey}
+                  geography={geo}
+                  style={{
+                    default: {
+                      fill: "#ECEFF1",
+                      stroke: "#607D8B",
+                      strokeWidth: 0.75,
+                      outline: "none",
+                    },
+                    hover: {
+                      fill: "#CFD8DC",
+                      stroke: "#607D8B",
+                      strokeWidth: 1,
+                      outline: "none",
+                    },
+                    pressed: {
+                      fill: "#FF5722",
+                      stroke: "#607D8B",
+                      strokeWidth: 1,
+                      outline: "none",
+                    },
+                  }}
+                  onClick={this.handleGeographyClick}
+                />
+              ))
+            }
+          </Geographies>
+        </ComposableMap>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default Map;
